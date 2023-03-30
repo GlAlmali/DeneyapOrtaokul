575df1fefa0ba62a5ff944ebaa2dd4e6f1665edb
GlAlmali <gul.almali.58@gmail.com>
2023-03-30 22:05:11 +0300

    Dosya eklendi

---
+++ Hafta4/CiktilariKarsilastirma/CiktilariKarsilastirma.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ad, soyad;
+    int yas;
+    cout <<"Adiniz: ";
+    cin >> ad;
+    cout <<"Soyadiniz:";
+    cin >> soyad;
+    cout <<"Yasiniz:";
+    cin >> yas;
+    return 0;
+}
